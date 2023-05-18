1451a95748e35751b503299fe231be282d6db19f
subramanian-s-043 <deivavijay61@gmail.com>
2023-05-18 12:27:53 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,37 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+// import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+// import CheckroomIcon from '@mui/icons-material/Checkroom';
+// import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+// import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'SNS Instituions', icon: <CodeIcon />, },
+  { name: 'SNS Square', icon: <CodeIcon />, },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'English Songs', icon: <MusicNoteIcon /> },
+  { name: 'Tamil Songs', icon: <MusicNoteIcon /> },
+  { name: 'Hindi Songs', icon: <MusicNoteIcon /> },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://snscourseware.org/images/SNS%20Institutionsapp.png';
+export const demoChannelUrl = '/channel/UCh5_2rB7jEA3PP0mgppk1vg';
+export const demoVideoUrl = '/video/ZO1aaOYSONI';
+export const demoChannelTitle = 'Square Media';
+export const demoVideoTitle = 'SNSCE';
+export const demoProfilePicture = 'https://snscourseware.org/images/SNS%20Institutionsapp.png'
